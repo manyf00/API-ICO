e162a8cc763a0fb451ab984cac6bbd4c3d0323c9
Manel <manelfemar@gmail.com>
2020-05-14 17:04:02 +0200

    Revert "Entidad-Relacion"
    This reverts commit c242d3a3f977c3f69c9eba6957bdf2bc2024034b.

--- src/Entity/Usuarios.php
+++ src/Entity/Usuarios.php
@@ -42,11 +42,6 @@ class Usuarios
      */
     private $telefonos = [];
 
-    /**
-     * @ORM\ManyToOne(targetEntity=Hospital::class, inversedBy="usuarios")
-     */
-    private $hospital;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -108,18 +103,6 @@ class Usuarios
     public function setTelefonos(?array $telefonos): self
     {
         $this->telefonos = $telefonos;
-
-        return $this;
-    }
-
-    public function getHospital(): ?Hospital
-    {
-        return $this->hospital;
-    }
-
-    public function setHospital(?Hospital $hospital): self
-    {
-        $this->hospital = $hospital;
 
         return $this;
     }
